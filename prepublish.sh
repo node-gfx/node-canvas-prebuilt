7b85bf1351710c703618bca9235174a6b7f9e970
Caleb Hearon <chearon@moresteam.com>
2016-10-31 17:29:57 -0400

    prepublish must be told which version now

--- prepublish.sh
+++ prepublish.sh
@@ -1,6 +1,11 @@
-npm install --ignore-scripts canvas@$npm_package_version && {
-  rm -rf canvas;
-  mv node_modules/canvas canvas;
-  rm -r node_modules;
+if [ "$1" = "" ]; then
+  echo "Specify which version"
+  exit 1
+fi
+
+npm install --ignore-scripts canvas@$1 && {
+  rm -rf canvas
+  mv node_modules/canvas canvas
+  rm -r node_modules
 }
 
