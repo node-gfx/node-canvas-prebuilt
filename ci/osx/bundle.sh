ab59e55f374116dedbae3ef42a83fe44cd61966a
Caleb Hearon <crh0872@gmail.com>
2017-03-30 22:30:17 -0400

    travis uses python 3.6 now?

--- ci/osx/bundle.sh
+++ ci/osx/bundle.sh
@@ -1,4 +1,4 @@
 build=node_modules/canvas/build/Release
 
-~/Library/Python/3.5/bin/macpack node_modules/canvas/build/Release/canvas.node -d .
+~/Library/Python/3.6/bin/macpack node_modules/canvas/build/Release/canvas.node -d .
 
