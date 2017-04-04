565c9383f763fb5a9d1e5b93cfd02106e29740d3
Caleb Hearon <caleb@chearon.net>
2017-04-03 23:17:14 -0400

    update prepublish to properly handle dependencies
    i had to unpublish again because some of them did not have nested
    dependencies introduced in canvas 1.6.5

--- prepublish.sh
+++ prepublish.sh
@@ -7,5 +7,11 @@ npm install --ignore-scripts canvas@$1 && {
   rm -rf canvas
   mv node_modules/canvas canvas
   rm -r node_modules
+
+  # should force to put dependencies in own node_modules, we don't want to have
+  # dependencies end up in some parent folder or they'll miss the release
+  cd canvas
+  npm install --ignore-scripts --production
+  cd ..
 }
 
