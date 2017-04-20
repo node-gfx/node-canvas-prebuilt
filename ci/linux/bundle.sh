121e943d2881335ef2a031aaf598fb1864f2fa2d
Caleb Hearon <caleb@chearon.net>
2017-04-19 22:17:16 -0400

    try something else

--- ci/linux/bundle.sh
+++ ci/linux/bundle.sh
@@ -1,5 +1,10 @@
 copies=$(lddtree.sh -l node_modules/canvas/build/Release/canvas.node | sed -r -e '/^\/lib/d' -e '/canvas.node$/d');
 
+# Travis and older versions of Ubuntu have glibs that are old and don't have
+# enough symbols for the other libraries that depend on it, so we have to force
+# it to be included even though it's somewhat of a system library
+copies="$copies $(lddtree.sh -l node_modules/canvas/build/Release/canvas.node | grep glib)"
+
 for so in $copies; do
   cp $so node_modules/canvas/build/Release
 done;
