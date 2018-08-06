84c8a6c52a057e8f430ec81e6b8f71d9fceb2226
Caleb Hearon <caleb@chearon.net>
2018-08-06 12:03:39 -0400

    update macOS build with Travis changes AGAIN

--- ci/osx/bundle.sh
+++ ci/osx/bundle.sh
@@ -1,4 +1,4 @@
 build=node-canvas/build/Release
 
-~/Library/Python/3.6/bin/macpack node-canvas/build/Release/canvas.node -d .
+~/Library/Python/*/bin/macpack node-canvas/build/Release/canvas.node -d .
 
