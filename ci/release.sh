61558956a50a9700710ebae17e2348c43c270ac6
Caleb Hearon <caleb@chearon.net>
2018-08-06 12:40:35 -0400

    node-pre-gyp uses "unknown" libc for macos (windows?)
    And detect-libc uses ''

--- ci/release.sh
+++ ci/release.sh
@@ -1,7 +1,7 @@
 # Generate the node-gyp formatted filename from the node environment
 FILENAME=$(
   node -e "
-    var p = process, v = p.versions, pkg = require('./package.json'), libc = require('detect-libc').family;
+    var p = process, v = p.versions, pkg = require('./package.json'), libc = require('detect-libc').family || 'unknown';
     console.log([pkg.name, 'v$CANVAS_PREBUILT_VERSION', 'node-v' + v.modules, p.platform, libc, p.arch].join('-'));
   "
 ).tar.gz;
